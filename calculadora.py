be037a7cdee01fcdd0ed06b32cfa9eed5c3e5a5f
Santy150429 <santiagorojasbuitrago980@gmail.com>
2026-09-21 07:30:30 -0500

    sistema de calculadora simple

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def suma(numero1,numero2):
+    return numero1+numero2
+
+print(suma(3,8))
+
+def restar():
+    numero1=int(input("Ingresa el primer numero"))
+    numero2=int(input("Ingresa el segundo numero"))
+    print(numero1-numero2)
+restar(10,3)
+
+def multiplicar():
+    numero1=int(input("ingrese numero1"))
+    numero2=int(input("ingrese numero2"))
+    print(numero1*numero2)
+multiplicar(4,3)
